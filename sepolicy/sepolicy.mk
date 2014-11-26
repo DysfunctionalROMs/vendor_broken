3c0e60216abd1c072c63ab0c8fa2fdc5751b1545
Ricardo Cerqueira <ricardo@cyngn.com>
2014-11-26 10:33:02 -0800

    Allow SystemServer to set service.adb.tcp.* properties
    Required for network adb enable/disable to function
    
    Change-Id: I3e2aacb6b8e9b107dcd229187a5dd76128e20001
    
    Conflicts:
    	sepolicy/sepolicy.mk

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -10,12 +10,15 @@ BOARD_SEPOLICY_UNION += \
     file.te \
     file_contexts \
     genfs_contexts \
+    property_contexts \
     seapp_contexts \
     auditd.te \
     healthd.te \
     installd.te \
     netd.te \
+    property.te \
     recovery.te \
+    shell.te \
     sysinit.te \
     system.te \
     ueventd.te \
