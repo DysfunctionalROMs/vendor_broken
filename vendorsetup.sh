53274a7c245eee1b56656ff0db1c297172c6016e
ken greenwood <kengreenwood420666@gmail.com>
2015-01-06 12:41:34 -0600

    cleaning it up

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,12 +4,10 @@ add_lunch_combo broken_d851-userdebug
 add_lunch_combo broken_d852-userdebug
 add_lunch_combo broken_deb-userdebug
 add_lunch_combo broken_dlx-userdebug
-add_lunch_combo broken_exhilarate-userdebug
 add_lunch_combo broken_falcon-userdebug
 add_lunch_combo broken_flo-userdebug
 add_lunch_combo broken_grouper-userdebug
 add_lunch_combo broken_hammerhead-userdebug
-add_lunch_combo broken_jflte-userdebug
 add_lunch_combo broken_l900-userdebug
 add_lunch_combo broken_lt01wifi-userdebug
 add_lunch_combo broken_m7vzw-userdebug
