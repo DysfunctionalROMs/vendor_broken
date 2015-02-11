deb92bf14bf8495c87f18fa1c429fb4024a507e2
ken greenwood <kengreenwood420666@gmail.com>
2015-02-11 08:44:05 -0600

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -29,3 +29,4 @@ add_lunch_combo broken_toro-userdebug
 add_lunch_combo broken_trltespr-userdebug 
 add_lunch_combo broken_ville-userdebug
 add_lunch_combo broken_vs980-userdebug
+add_lunch_combo broken_w7-userdebug
