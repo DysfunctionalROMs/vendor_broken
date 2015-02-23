LiquidSmooth-Old/android_vendor_liquid

Add basic Layers Permission and prebuilt apk
Add this to the apps androidmanifest.xml
http://pastebin.com/28DFf4Wf

Stop CM theme engine from building aswell

Change-Id: I5a38757a78341bc1bbe448ac1b9b0abb1d61f468

config/common.mk
@@ -110,8 +110,13 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/liquid/config/permissions/com.liquidsmooth.android.xml:system/etc/permissions/com.liquidsmooth.android.xml
 
+# Layers Theme
+PRODUCT_COPY_FILES += \
+    vendor/liquid/config/permissions/com.layers.theme.xml:system/etc/permissions/com.layers.theme.xml
+    vendor/liquid/prebuilt/common/etc/Layers.apk:system/app/Layers/Layers.apk
+
 # T-Mobile theme engine
-include vendor/liquid/config/themes_common.mk
+#include vendor/liquid/config/themes_common.mk
 
 # Liquid Stuff
 PRODUCT_PACKAGES += \

DysfunctionalROMs/vendor_broken

Conflicts:
	config/common.mk

config/common.mk
@@ -70,6 +70,11 @@ PRODUCT_COPY_FILES += \
     vendor/broken/prebuilt/common/xbin/zip:system/xbin/zip \
     vendor/broken/prebuilt/common/xbin/zipalign:system/xbin/zipalign
 
+# Layers Theme
+PRODUCT_COPY_FILES += \
+    vendor/broken/config/permissions/com.layers.theme.xml:system/etc/permissions/com.layers.theme.xml \
+    vendor/broken/prebuilt/common/etc/Layers.apk:system/app/Layers/Layers.apk
+
 # Workaround for NovaLauncher zipalign fails
 PRODUCT_COPY_FILES += \
     vendor/broken/prebuilt/common/app/NovaLauncher.apk:system/app/NovaLauncher.apk
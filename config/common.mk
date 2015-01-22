048604f151916495f2d56d23d98b954dd40dbdeb
deadman96385 <seanhoyt963@gmail.com>
2015-01-22 12:18:21 -0500

    Add basic Layers Permission and prebuilt apk
    Add this to the apps androidmanifest.xml
    http://pastebin.com/28DFf4Wf
    
    Stop CM theme engine from building aswell
    
    Change-Id: I5a38757a78341bc1bbe448ac1b9b0abb1d61f468
    
    Conflicts:
    	config/common.mk

--- config/common.mk
+++ config/common.mk
@@ -70,6 +70,11 @@ PRODUCT_COPY_FILES += \
     vendor/broken/prebuilt/common/xbin/zip:system/xbin/zip \
     vendor/broken/prebuilt/common/xbin/zipalign:system/xbin/zipalign
 
+# Layers Theme
+PRODUCT_COPY_FILES += \
+    vendor/broken/config/permissions/com.layers.theme.xml:system/etc/permissions/com.layers.theme.xml \
+    vendor/broken/prebuilt/common/etc/Layers.apk:system/app/Layers/Layers.apk
+
 # Workaround for NovaLauncher zipalign fails
 PRODUCT_COPY_FILES += \
     vendor/broken/prebuilt/common/app/NovaLauncher.apk:system/app/NovaLauncher.apk
