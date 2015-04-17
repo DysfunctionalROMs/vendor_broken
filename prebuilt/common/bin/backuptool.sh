SlimRoms/vendor_slim

Slim: Update backuptool for 5.1
Change-Id: Icc0cc4f0dbd4945cd3ed03e9975df8f2c5d83c51

prebuilt/common/bin/backuptool.sh
@@ -5,7 +5,7 @@
 
 export C=/tmp/backupdir
 export S=/system
-export V=Slim-5.0
+export V=Slim-5.1
 
 # Preserve /system/addon.d in /tmp/addon.d
 preserve_addon_d() {

DysfunctionalROMs/vendor_broken

Conflicts:
	prebuilt/common/bin/backuptool.sh

prebuilt/common/bin/backuptool.sh
@@ -5,7 +5,7 @@
 
 export C=/tmp/backupdir
 export S=/system
-export V=Broken
+export V=Broken-5.1
 
 # Preserve /system/addon.d in /tmp/addon.d
 preserve_addon_d() {