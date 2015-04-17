SlimRoms/vendor_slim

Create config for sim enabled tablets and split out generic apn configurations
Change-Id: I6f953386dbc2592c230a849f1b7d6fd3df678add

config/common_apn.mk
@@ -1,0 +1,12 @@
+# World APN list
+PRODUCT_COPY_FILES += \
+    vendor/slim/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+# World SPN overrides list
+PRODUCT_COPY_FILES += \
+    vendor/slim/prebuilt/common/etc/spn-conf.xml:system/etc/spn-conf.xml
+
+# Selective SPN list for operator number who has the problem.
+PRODUCT_COPY_FILES += \
+    vendor/slim/prebuilt/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
+

DysfunctionalROMs/vendor_broken

Conflicts:
	config/common_full_phone.mk

config/common_apn.mk
@@ -1,0 +1,11 @@
+# World APN list
+PRODUCT_COPY_FILES += \
+    vendor/broken/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+# World SPN overrides list
+PRODUCT_COPY_FILES += \
+    vendor/broken/prebuilt/common/etc/spn-conf.xml:system/etc/spn-conf.xml
+
+# Selective SPN list for operator number who has the problem.
+PRODUCT_COPY_FILES += \
+    vendor/broken/prebuilt/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
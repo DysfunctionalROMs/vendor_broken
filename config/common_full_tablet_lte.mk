18cb0263a9b89fe0f0a1a89b99bfbe6f1ff65a66
Nicholas Flintham <nick@flinny.org>
2015-01-19 14:55:04 +0000

    Create config for sim enabled tablets and split out generic apn configurations
    Change-Id: I6f953386dbc2592c230a849f1b7d6fd3df678add
    
    Conflicts:
    	config/common_full_phone.mk

--- config/common_full_tablet_lte.mk
+++ config/common_full_tablet_lte.mk
@@ -1,7 +1,3 @@
 # Inherit common stuff
 $(call inherit-product, vendor/broken/config/common.mk)
 $(call inherit-product, vendor/broken/config/common_apn.mk)
-
-# SIM Toolkit
-PRODUCT_PACKAGES += \
-    Stk
