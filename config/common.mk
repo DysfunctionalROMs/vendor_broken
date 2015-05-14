namdeptrai2003/android_vendor_dnd

cm: Put install tools in OUT/install
 * This allows us to avoid modifying /system at install time.

Change-Id: I747551d7f38a3eef70ab64f32b6d4a4749c01012

config/common.mk
@@ -81,15 +81,15 @@ PRODUCT_COPY_FILES += \
 # Backup Tool
 ifneq ($(WITH_GMS),true)
 PRODUCT_COPY_FILES += \
-    vendor/cm/prebuilt/common/bin/backuptool.sh:system/bin/backuptool.sh \
-    vendor/cm/prebuilt/common/bin/backuptool.functions:system/bin/backuptool.functions \
+    vendor/cm/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
+    vendor/cm/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
     vendor/cm/prebuilt/common/bin/50-cm.sh:system/addon.d/50-cm.sh \
     vendor/cm/prebuilt/common/bin/blacklist:system/addon.d/blacklist
 endif
 
 # Signature compatibility validation
 PRODUCT_COPY_FILES += \
-    vendor/cm/prebuilt/common/bin/otasigcheck.sh:system/bin/otasigcheck.sh
+    vendor/cm/prebuilt/common/bin/otasigcheck.sh:install/bin/otasigcheck.sh
 
 # init.d support
 PRODUCT_COPY_FILES += \

DysfunctionalROMs/vendor_broken

Conflicts:
	config/common.mk

config/common.mk
@@ -28,8 +28,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Backup Tool
 PRODUCT_COPY_FILES += \
-    vendor/broken/prebuilt/common/bin/backuptool.sh:system/bin/backuptool.sh \
-    vendor/broken/prebuilt/common/bin/backuptool.functions:system/bin/backuptool.functions \
+    vendor/broken/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
+    vendor/broken/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
     vendor/broken/prebuilt/common/bin/50-broken.sh:system/addon.d/50-broken.sh \
     vendor/broken/prebuilt/common/bin/99-backup.sh:system/addon.d/99-backup.sh \
     vendor/broken/prebuilt/common/etc/backup.conf:system/etc/backup.conf