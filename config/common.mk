fa60b1646ae7198b7066460983dabe901844539e
InkSlinger666 <kengreenwood420666@gmail.com>
2015-07-08 21:47:37 -0500

    Hide that damn layers icon

--- config/common.mk
+++ config/common.mk
@@ -17,7 +17,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.android.wifi-watchlist=GoogleGuest \
     ro.setupwizard.enterprise_mode=1 \
     ro.com.android.dateformat=MM-dd-yyyy \
-    ro.com.android.dataroaming=false
+    ro.com.android.dataroaming=false \
+    ro.layers.noIcon=noIcon
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.selinux=1
