0a69bb11576f64fa16d93e08495f2415ff5b8751
ken greenwood <kengreenwood420666@gmail.com>
2016-03-22 08:11:42 -0500

    add kiwi

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -25,6 +25,7 @@ add_lunch_combo broken_grouper-userdebug
 add_lunch_combo broken_hammerhead-userdebug
 add_lunch_combo broken_hercules-userdebug
 add_lunch_combo broken_i605-userdebug
+add_lunch_combo broken_kiwi-userdebug
 add_lunch_combo broken_klte-userdebug
 add_lunch_combo broken_l900-userdebug
 add_lunch_combo broken_ls990-userdebug
