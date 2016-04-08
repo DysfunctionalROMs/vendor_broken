dcb4e52aafed94d4ae27b2df1afadb48e1e41c0b
InkSlinger666 <kengreenwood420666@gmail.com>
2016-04-08 15:53:09 -0500

    Fix import of apns for lte tablets

--- config/common_full_tablet_lte.mk
+++ config/common_full_tablet_lte.mk
@@ -1,3 +1,10 @@
 # Inherit common stuff
 $(call inherit-product, vendor/broken/config/common.mk)
-$(call inherit-product, vendor/broken/config/common_apn.mk)
+
+# World APN list
+PRODUCT_COPY_FILES += \
+    vendor/broken/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+# Selective SPN list for operator number who has the problem.
+PRODUCT_COPY_FILES += \
+    vendor/broken/prebuilt/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
