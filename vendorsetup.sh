77676a292ed2394ccba04f196b95220b73f85328
dfuse06 <dfuse06@gmail.com>
2016-07-05 07:47:53 -0500

    Good bye bitches (draconis)
    Change-Id: I7ec9e1c761cc833e9e4328b8a8d756ce5cdbb246

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -15,7 +15,6 @@ add_lunch_combo broken_d851-userdebug
 add_lunch_combo broken_d852-userdebug
 add_lunch_combo broken_d855-userdebug
 add_lunch_combo broken_deb-userdebug
-add_lunch_combo broken_draconis-userdebug
 add_lunch_combo broken_f400-userdebug
 add_lunch_combo broken_falcon-userdebug
 add_lunch_combo broken_flo-userdebug
