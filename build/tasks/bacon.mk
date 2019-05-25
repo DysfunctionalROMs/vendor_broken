f22876e1f6df2af7cf66479c28cc80da25890646
TeamFahQ <TeamFahQ>
2019-05-25 13:07:03 -0500

    Brokenize some more
    Change-Id: Ia21d0a458d981cbec4db3ae5087730fa3d03b5c6

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -19,8 +19,14 @@
 # Build colors
 
 ifneq ($(BUILD_WITH_COLORS),0)
-  CL_BROKEN="\033[38;5;24m"
+  CL_SLIM="\033[38;5;24m"
   CL_SGRY="\033[38;5;239m"
+  CL_RED="\033[31m"
+  CL_GRN="\033[32m"
+  CL_YLW="\033[33m"
+  CL_BLU="\033[34m"
+  CL_MAG="\033[35m"
+  CL_CYN="\033[36m"
   CL_RST="\033[0m"
 endif
 
@@ -29,18 +35,33 @@ BROKEN_TARGET_PACKAGE := $(PRODUCT_OUT)/$(BROKEN_MOD_VERSION).zip
 .PHONY: bacon
 bacon: $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(BROKEN_TARGET_PACKAGE)
-	$(hide) $(MD5SUM) $(BROKEN_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(BROKEN_TARGET_PACKAGE).md5sum
-	@echo -e " "
-	@echo -e ${CL_BROKEN}" "${CL_BROKEN}
-	@echo -e ${CL_BROKEN}"                              _______________________________________________  "${CL_BROKEN}
-	@echo -e ${CL_BROKEN}"                             /                                  brokenroms.org | "${CL_BROKEN}
-	@echo -e ${CL_BROKEN}"                            /   _____________________________________________| "${CL_BROKEN}
-	@echo -e ${CL_BROKEN}"                           /   /                                               "${CL_BROKEN}
-	@echo -e ${CL_BROKEN}"                          /   /  _  _      "${CL_SGRY}" ______                             "${CL_BROKEN}
-	@echo -e ${CL_BROKEN}"                         /   /  | |(_)     "${CL_SGRY}"(_____ \                            "${CL_BROKEN}
-	@echo -e ${CL_BROKEN}"                        /   /   | | _ _____ "${CL_SGRY}"_____) )___  _____  ___            "${CL_BROKEN}
-	@echo -e ${CL_BROKEN}"  _____________________/   /    | || |     |"${CL_SGRY}"  __  // _ \|     |/___)           "${CL_BROKEN}
-	@echo -e ${CL_BROKEN}" |                        /     | || | | | |"${CL_SGRY}" |  \ \ |_| | | | |___ |           "${CL_BROKEN}
-	@echo -e ${CL_BROKEN}" |_______________________/       \_)_|_|_|_|"${CL_SGRY}"_|   |_\___/|_|_|_(___/            "${CL_BROKEN}
-	@echo -e ${CL_BROKEN}" "${CL_BROKEN}
+	$(hide) $(MD5SUM) $(BROKEN_TARGET_PACKAGE) > $(BROKEN_TARGET_PACKAGE).md5sum
+	@echo -e ${CL_CYN}"   @@@@@@@   @@@@@@@    @@@@@@   @@@  @@@  @@@@@@@@  @@@  @@@   "${CL_CYN}
+	@echo -e ${CL_CYN}"   @@@@@@@@  @@@@@@@@  @@@@@@@@  @@@  @@@  @@@@@@@@  @@@@ @@@   "${CL_CYN}
+	@echo -e ${CL_CYN}"   @@!  @@@  @@!  @@@  @@!  @@@  @@!  !@@  @@!       @@!@!@@@   "${CL_CYN}
+	@echo -e ${CL_CYN}"   !@   @!@  !@!  @!@  !@!  @!@  !@!  @!!  !@!       !@!!@!@!   "${CL_CYN}
+	@echo -e ${CL_CYN}"   @!@!@!@   @!@!!@!   @!@  !@!  @!@@!@!   @!!!:!    @!@ !!@!   "${CL_CYN}
+	@echo -e ${CL_CYN}"   !!!@!!!!  !!@!@!    !@!  !!!  !!@!!!    !!!!!:    !@!  !!!   "${CL_CYN}
+	@echo -e ${CL_CYN}"   !!:  !!!  !!: :!!   !!:  !!!  !!: :!!   !!:       !!:  !!!   "${CL_CYN}
+	@echo -e ${CL_CYN}"   :!:  !:!  :!:  !:!  :!:  !:!  :!:  !:!  :!:       :!:  !:!   "${CL_CYN}
+	@echo -e ${CL_CYN}"   :: ::::   ::   :::  ::::: ::   ::  :::   :: ::::   ::   ::   "${CL_CYN}
+	@echo -e ${CL_CYN}"   :: : ::    :   : :   : :  :    :   :::  : :: ::   ::    :    "${CL_CYN}
+	@echo -e ${CL_CYN}"                                                                "${CL_CYN}
+	@echo -e ${CL_CYN}"                        @@@@@@    @@@@@@                        "${CL_CYN}
+	@echo -e ${CL_CYN}"                       @@@@@@@@  @@@@@@@                        "${CL_CYN}
+	@echo -e ${CL_CYN}"                       @@!  @@@  !@@                            "${CL_CYN}
+	@echo -e ${CL_CYN}"                       !@!  @!@  !@!                            "${CL_CYN}
+	@echo -e ${CL_CYN}"                       @!@  !@!  !!@@!!                         "${CL_CYN}
+	@echo -e ${CL_CYN}"                       !@!  !!!   !!@!!!                        "${CL_CYN}
+	@echo -e ${CL_CYN}"                       !!:  !!!       !:!                       "${CL_CYN}
+	@echo -e ${CL_CYN}"                       :!:  !:!      !:!                        "${CL_CYN}
+	@echo -e ${CL_CYN}"                       ::::: ::  :::: ::                        "${CL_CYN}
+	@echo -e ${CL_CYN}"                        : :  :   :: : :                         "${CL_CYN}
+	@echo -e ${CL_CYN}"                                                                "${CL_CYN}
+	@echo -e ${CL_YLW}"                    Its Done     Getcha Some                    "${CL_YLW}
+	@echo -e ${CL_YLW}"                          #StayBroken                           "${CL_YLW}
+	@echo -e ${CL_RST}""${CL_RST}
+	@echo -e ${CL_BLU}"================================================================"${CL_BLU}
+	@echo -e ${CL_RST}"$(BROKEN_TARGET_PACKAGE)"${CL_RST}
+	@echo -e ${CL_BLU}"================================================================"${CL_BLU}
 	@echo -e ${CL_CYN}"Package Complete: $(BROKEN_TARGET_PACKAGE)"${CL_RST}
